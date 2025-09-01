8005e20e0c620611981eeef2d2cb4b2d3699b27a
sifat-samin <sifatsamin19@gmail.com>
2025-09-01 13:40:37 +0600

    Deployment Ready

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/GiftStore/',
+  base: '/Gift_Shop/',
   plugins: [
     react(),
     tailwindcss()
